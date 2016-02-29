a4588dbd9cf2fa7b45809e6d194fee338ffea61f
AlessioMarinelli-ANTOS <alessio.marinelli@antos.it>
2016-02-29 13:05:19 +0100

    BRV-3051 completed

---
+++ Test-GIT-VS/Test-GIT-VS/BRV-3051.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Test_GIT_VS
+{
+    public partial class BRV_3051 : Form
+    {
+        public BRV_3051()
+        {
+            InitializeComponent();
+        }
+    }
+}
